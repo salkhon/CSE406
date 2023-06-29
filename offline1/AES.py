43edf07ec59690a8675456b8465b241bf282388e
salkhon <salmankhondker1998@gmail.com>
2023-06-29 20:23:57 +0600

    before using numpy

--- offline1/AES.py
+++ offline1/AES.py
@@ -1,3 +1,9 @@
+from collections.abc import Callable
+from typing import Any, TypeVar
+from BitVector import BitVector
+
+T = TypeVar("T")
+
 
 class AES:
     """Symmetric cryptography system. 
@@ -40,10 +46,36 @@ class AES:
         0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,
     )
 
+    MIXER = [
+        [BitVector(hexstring="02"), BitVector(hexstring="03"),
+         BitVector(hexstring="01"), BitVector(hexstring="01")],
+        [BitVector(hexstring="01"), BitVector(hexstring="02"),
+         BitVector(hexstring="03"), BitVector(hexstring="01")],
+        [BitVector(hexstring="01"), BitVector(hexstring="01"),
+         BitVector(hexstring="02"), BitVector(hexstring="03")],
+        [BitVector(hexstring="03"), BitVector(hexstring="01"),
+         BitVector(hexstring="01"), BitVector(hexstring="02")]
+    ]
+
+    INV_MIXER = [
+        [BitVector(hexstring="0E"), BitVector(hexstring="0B"),
+         BitVector(hexstring="0D"), BitVector(hexstring="09")],
+        [BitVector(hexstring="09"), BitVector(hexstring="0E"),
+         BitVector(hexstring="0B"), BitVector(hexstring="0D")],
+        [BitVector(hexstring="0D"), BitVector(hexstring="09"),
+         BitVector(hexstring="0E"), BitVector(hexstring="0B")],
+        [BitVector(hexstring="0B"), BitVector(hexstring="0D"),
+         BitVector(hexstring="09"), BitVector(hexstring="0E")]
+    ]
+
+    MODULUS = BitVector(bitstring='100011011')
+
+    RC = (0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36)
+
     def __init__(self):
         pass
 
-    def encrypt(self, text: str, key: str) -> str:
+    def encrypt(self, plaintext16: str, key: str) -> str:
         """Encrypt the given text, with the provided key.
 
         If key has length less than 128 bits, zero pad it. 
@@ -55,7 +87,128 @@ class AES:
         Returns:
             str: Cipher
         """
+        if len(key) > 16:
+            key = key[:16]
+
+        # chunk or pad text to list 16 char parts
+        plaintexts = self.convert_text_to_16_bytes(plaintext16)
+
+        # get list of round key matrices
+        round_keys_mats4x4 = self.get_round_keys_matrices(key)
+
+        for plaintext16 in plaintexts:
+            # state matric for plain text chunk
+            state_mat4x4 = self.convert_list16_to_mat4x4(
+                self.convert_str_to_int_list(plaintext16))
+
+            for round in range(10):
+                self.compute_round(
+                    state_mat4x4, round_keys_mats4x4[round], round)
+
         return ""
+
+    def compute_round(self, state_mat4x4: list[list[int]], round_key_mat4x4: list[list[int]], round: int) -> list[list[int]]:
+        """Compute each round operation on state matrix.
+
+        Args:
+            state_mat4x4 (list[list[int]]): State matrix of this round
+            round_key_mat4x4 (list[list[int]]): Round key matrix for this round
+            round (int): This round
+
+        Returns:
+            list[list[int]]: Computed state matrix after this round
+        """
+        # add roundkey
+        state_mat = self.apply_elemwise4x4(
+            state_mat4x4, round_key_mat4x4, lambda x, y: x ^ y)
+
+        # byte substitution
+        state_mat = [[AES.SBOX[b] for b in sublist] for sublist in state_mat]
+
+        # shift row
+        for row in range(1, 4):
+            state_mat[row] = self.left_rotate_word(state_mat[row], amount=row)
+
+        # mix column
+        state_mat = [[BitVector(intVal=i) for i in row] for row in state_mat]
+        state_mat = self.apply_elemwise4x4(
+            AES.MIXER, state_mat, lambda x, y: x.gf_multiply_modular(y, AES.MODULUS, 8))
+        state_mat = [[bv.int_val() for bv in row] for row in state_mat]
+
+        return state_mat
+
+    def apply_matmulwise4x4(self, A: list[list[T]], B: list[list[T]],
+                            func: Callable[[T, T], T]) -> list[list[T]]:
+        for i in range(4):
+            for j in range(4):
+                for k in range(4):
+                    pass
+
+    def apply_elemwise4x4(self, A: list[list[T]], B: list[list[T]],
+                          func: Callable[[T, T], T]) -> list[list[T]]:
+        """Compute element-wise operation for two matrices.
+
+        Args:
+            A (list[list[int]]): Matrix A
+            B (list[list[int]]): Matrix B
+
+        Returns:
+            list[list[int]]: result matrix
+        """
+        res: list[list[T]] = []
+        for i in range(4):
+            res.append([])
+            for j in range(4):
+                res[i].append(func(A[i][j], B[i][j]))
+        return res
+
+    def get_round_keys_matrices(self, key: str) -> list[list[list[int]]]:
+        """Generate round keys from provided key using KEY EXPANSION, and return a list of matrices that are the
+        column major matrices suitable for encryption.
+
+        Args:
+            key: str: Key string
+
+        Returns:
+            list[list[list[int]]]: List of 4x4 column major matrices of each key
+        """
+        # List of 4x4 round key mats, for each round
+        round_keys_mats4x4: list[list[list[int]]] = []
+
+        # Convert key words to matrices
+        round_keys = self.key_expansion(key)
+        for i in range(0, 44, 4):
+            # get 4 words at a time to get 16 byte list
+            round_keys_mats4x4.append(self.convert_list16_to_mat4x4(
+                self.flatten_list_of_list(round_keys[i:i+4])))
+
+        return round_keys_mats4x4
+
+    def flatten_list_of_list(self, list_o_list: list[list]) -> list:
+        """Convert list of list to list. 
+
+        Args:
+            list_o_list (list[list]): List of list
+
+        Returns:
+            list: Flattened list
+        """
+        return [elem for sublist in list_o_list for elem in sublist]
+
+    def convert_list16_to_mat4x4(self, list16: list) -> list[list]:
+        """Convert a list of 16 elements to 4x4 matrix format, in **column major** order. 
+
+        Args:
+            list16 (list): List of 16 elements
+
+        Returns:
+            list[list]: Column major matrix 4x4
+        """
+        mat = [[0] * 4 for row in range(4)]
+        for idx, elem in enumerate(list16):
+            mat[idx % 4][idx // 4] = elem
+
+        return mat
 
     def decrypt(self, cipher: str, key: str) -> str:
         """Decrpypt the given cipher with the given key.
@@ -69,29 +222,30 @@ class AES:
         """
         return ""
 
-    def convert_to_16_byte_keys(self, key: str) -> list[str]:
-        """Convert key of any length to list of 16 bytes keys (128 bits).
+    def convert_text_to_16_bytes(self, plaintext: str) -> list[str]:
+        """Convert text of any length to list of 16 bytes textx (128 bits).
 
         Args:
-            key (str): Key of any length
+            text (str): text of any length
 
         Returns:
-            list[str]: List of 16 byte keys
+            list[str]: List of 16 byte texts
         """
 
-        def pad_smaller_key(key: str) -> str:
-            return '\0' * (16 - len(key)) + key
+        def pad_smaller_text(text: str) -> str:
+            return '\0' * (16 - len(text)) + text
 
-        keys = []
-        if len(key) < 16:
-            keys.append(pad_smaller_key(key))
-        elif len(key) > 16:
-            keys.extend([key[i:i+16] for i in range(0, len(key) - 16, 16)])
-            keys.append(
-                pad_smaller_key(
-                    key[(len(key) // 16) * 16:]))
+        texts = []
+        if len(plaintext) < 16:
+            texts.append(pad_smaller_text(plaintext))
+        elif len(plaintext) > 16:
+            texts.extend([plaintext[i:i+16]
+                         for i in range(0, len(plaintext) - 16, 16)])
+            texts.append(
+                pad_smaller_text(
+                    plaintext[(len(plaintext) // 16) * 16:]))
 
-        return keys
+        return texts
 
     def convert_16_byte_key_to_4_byte_words(self, key: str) -> list[str]:
         """Convert 16 bytes key (HAS TO BE 16 BYTE KEY) to a list of 4 byte words.
@@ -104,38 +258,98 @@ class AES:
         """
         return [key[i:i+4] for i in range(0, len(key), 4)]
 
-    def key_expansion(self, key: str) -> list[str]:
+    def convert_str_to_int_list(self, key: str) -> list[int]:
+        """Convert string to list of unicode ints.
+
+        Args:
+            key (str): Key string
+
+        Returns:
+            list[int]: list of UNICODE ints of the chars
+        """
+        return [ord(c) for c in key]
+
+    def left_rotate_word(self, word: list[int], amount=1) -> list[int]:
+        """Left rotate word by 1. `word` must have length greater than 1.
+
+        Args:
+            word (list[int]): word to be rotated
+
+        Returns:
+            str: Left rotated word
+        """
+        return word[amount:] + word[0:amount]
+
+    def get_next_round_key(self, key: list[list[int]], round: int) -> list[list[int]]:
+        """Get next round's key from provided key.
+
+        Args:
+            key: list[list[int]]: 4x4 matrix of words in the key
+            round: int: The round number
+
+        Returns:
+            list[list[int]]: The 4x4 matrix for the next round's key
+        """
+
+        def xor_words(word1: list[int], word2: list[int]) -> list[int]:
+            """Elementwise XOR between two key words. 
+
+            Args:
+                word1 (list[int]): Word 1
+                word2 (list[int]): Word 2
+
+            Returns:
+                list[int]: Resulting word after XOR 
+            """
+            return list(map(lambda x, y: x ^ y, word1, word2))
+
+        w0, w1, w2, w3 = key
+        gw3 = self.left_rotate_word(w3)
+
+        # byte substitution
+        gw3 = [AES.SBOX[b] for b in gw3]
+
+        # Adding round constant (XORing 1 to left most char of gw3)
+        gw3[0] ^= AES.RC[round]
+
+        # next key
+        w4 = xor_words(w0, gw3)
+        w5 = xor_words(w4, w1)
+        w6 = xor_words(w5, w2)
+        w7 = xor_words(w6, w3)
+
+        return [w4, w5, w6, w7]
+
+    def key_expansion(self, key: str) -> list[list[int]]:
         """Expand 16 byte key to 44 keys each of 4 words.
 
         Args:
             key (str): Key string of 16 characters
 
         Returns:
-            list[str]: List of 44 4-characted keys
+            list[list[int]]: List of 44 4-byte words
         """
 
-        def left_rotate_4_byte_word(word: str) -> str:
-            """Left rotate 4 character word.
-
-            Args:
-                word (str): 4 character word
-
-            Returns:
-                str: Left rotated word
-            """
-            return word[1:] + word[0]
-
         words = self.convert_16_byte_key_to_4_byte_words(key)
-        gw4 = left_rotate_4_byte_word(words[3])
+        words = [self.convert_str_to_int_list(word) for word in words]
 
-        # byte substitution
-        gw4 = "".join(
-            [chr(AES.SBOX[ord(c)])
-             for c in gw4])
+        # list of words (list[int])
+        key_words: list[list[int]] = [*words]
 
-        return []
+        # todo: variable round of keys for variable keys
+        for round in range(10):
+            next_words = self.get_next_round_key(words, round)
+            key_words.extend(next_words)
+            words = next_words
+
+        return key_words
+
+    def convert_word_to_hex(self, word: list[int]) -> list[str]:
+        return [hex(w) for w in word]
 
 
 if __name__ == "__main__":
     aes = AES()
-    print(aes.convert_to_16_byte_keys("abcdefghijkl" * 3))
+    words = aes.key_expansion("Thats my Kung Fu")
+    for i in range(44):
+        print(aes.convert_word_to_hex(words[i]))
